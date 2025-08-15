3c099346830bde60f88b0c47b428470cd4cf4b94
Josh Segeleski <joshsegeleski@Mac.ht.home>
2025-08-15 09:39:05 -0600

    Allow disabling Shadow DOM for theme styling; use theme button classes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
+          { key: "X-Robots-Tag", value: "noindex, nofollow, noarchive" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS,HEAD" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type" },
